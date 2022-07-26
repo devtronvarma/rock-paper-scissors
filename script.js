174a0c63729118083f8554f355d8eca13261c7b8
Dev Varma <devtronvarma@gmail.com>
2022-07-25 21:50:13 -0500

    Add helpful comments and code to run game

--- script.js
+++ script.js
@@ -12,34 +12,43 @@ function getComputerSelection() {
 
 // Function to get input from user
 function getUserSelection() {
+    // Prompt user to input one of three prescribed choices
     let userSelection = prompt("Rock, paper, or scissors?").toLowerCase();
     return userSelection;
 }
 
 // Function to determine winner between user and computer
 function playRound() {
+    // Initiate variables for comparisons and output message
     let computerSelection = getComputerSelection();
     let userSelection = getUserSelection();
     let message = "";
 
+    // Check for ties
     if (computerSelection == userSelection) {
         message =  `It's a tie. You both picked ${userSelection}.`;
     }
 
+    // Check for computer winning
     else if ((computerSelection == "rock" && userSelection == "scissors") ||(computerSelection == "paper" && userSelection == "rock") || (computerSelection == "scissors" && userSelection == "paper") ) {
         message = `You lose! ${computerSelection} beats ${userSelection}.`;  
     }
 
+    // Otherwise, user wins
     else {
         message = `You win! ${userSelection} beats ${computerSelection}.`;
     }
 
+    // Return message (which side won)
     return message;
 }
 
-// Function to iterate playRound for 5 rounds: game ()
+// Function to iterate playRound for 5 rounds
 function game() {
     for (i = 0; i < 5; i++) {
         console.log(playRound())
     }
 }
+
+// Call game to initiate game play
+game();
